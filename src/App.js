cecf83a7dc2e1087ae8c1d76220a0dc22f6c1bf0
Jesse Owens <jesseowens777@yahoo.com>
2021-01-05 18:34:31 -0800

    changed welcome message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome dude
         </a>
       </header>
     </div>
